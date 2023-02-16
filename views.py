55b0dfa31bda31b094c475e930a8f01d5167f37b
Josdelsan <joseM_DS@hotmail.com>
2023-02-17 00:57:00 +0100

    New msg

--- views.py
+++ views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def authPageView(request):
-    return HttpResponse("Hello, World from Auth!")
+    return HttpResponse("Hello, World from Auth!!")
